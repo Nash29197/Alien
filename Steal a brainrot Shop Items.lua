7b1702baada950c67348599183541ab6842e8ee3
Nash29197 <Sx508375@gmail.com>
2025-07-13 12:14:15 +0800

    Create Steal a brainrot Shop Items.lua

---
+++ Steal a brainrot Shop Items.lua
@@ -0,0 +1,8 @@
+return {
+    "Slap", "Speed Coil", "Trap", "Iron Slap", "Gravity Coil", "Bee Launcher", "Gold Slap",
+    "Coil Combo", "Rage Table", "Diamond Slap", "Grapple Hook", "Taser Gun", "Emerald Slap",
+    "Invisibility Cloak", "Boogie Bomb", "Ruby Slap", "Medusa's Head", "Dark Matter Slap",
+    "Web Slinger", "Flame Slap", "Quantum Cloner", "All Seeing Sentry", "Nuclear Slap",
+    "Rainbowrath Sword", "Galaxy Slap", "Laser Cape", "Glitched Slap", "Body Swap Potion",
+    "Splatter Slap", "Painball Gun",
+}
