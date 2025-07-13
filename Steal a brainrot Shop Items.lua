2feea89c71ccedf739c59da21d5345b68896635c
Nash29197 <Sx508375@gmail.com>
2025-07-13 12:25:24 +0800

    Update Steal a brainrot Shop Items.lua

--- Steal a brainrot Shop Items.lua
+++ Steal a brainrot Shop Items.lua
@@ -4,5 +4,5 @@ return {
     "Invisibility Cloak", "Boogie Bomb", "Ruby Slap", "Medusa's Head", "Dark Matter Slap",
     "Web Slinger", "Flame Slap", "Quantum Cloner", "All Seeing Sentry", "Nuclear Slap",
     "Rainbowrath Sword", "Galaxy Slap", "Laser Cape", "Glitched Slap", "Body Swap Potion",
-    "Splatter Slap", "Painball Gun",
+    "Splatter Slap", "Painball Gun","Heart Balloon","Magnet",
 }
