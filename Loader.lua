7286f56cc4604766cf124d93b5ded01c47014fea
三眼怪 <Sx508375@gmail.com>
2025-11-29 19:16:11 +0800

    Add script loading for specific game PlaceId

---
+++ Loader.lua
@@ -0,0 +1,4 @@
+repeat task.wait() until game:IsLoaded()
+if game.PlaceId == 127742093697776 then
+    loadstring(game:HttpGet("https://scriptprotector.vercel.app/api/raw/fb7424dd3c0489db62fbab92adef599c"))()
+end
