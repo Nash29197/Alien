06e50dc090b3d1960d3618677ac27caf26e6261b
三眼怪 <Sx508375@gmail.com>
2025-11-29 19:30:52 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,10 @@
 repeat task.wait() until game:IsLoaded()
-if game.PlaceId == 127742093697776 then ---Plants Vs Brainrots
+if setfpscap then setfpscap(240) end
+
+loadstring(game:HttpGet("https://scriptprotector.vercel.app/api/raw/b73f255ad93a6f2e65a09ad4b102753d"))()
+
+if game.PlaceId == 127742093697776 then
     loadstring(game:HttpGet("https://scriptprotector.vercel.app/api/raw/fb7424dd3c0489db62fbab92adef599c"))()
-elseif game.PlaceId == 136801880565837 then ---[FPS] FLICK
+elseif game.PlaceId == 136801880565837 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Gibssss-art/vuln/refs/heads/main/Flick"))()
 end
