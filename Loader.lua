0430a9bb3c2335abad2474a281b6a113bc00af12
三眼怪 <Sx508375@gmail.com>
2025-11-29 19:21:40 +0800

    Update and rename Loader to Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,6 @@
+repeat task.wait() until game:IsLoaded()
+if game.PlaceId == 127742093697776 then ---Plants Vs Brainrots
+    loadstring(game:HttpGet("https://scriptprotector.vercel.app/api/raw/fb7424dd3c0489db62fbab92adef599c"))()
+elseif game.PlaceId == 136801880565837 then ---[FPS] FLICK
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gibssss-art/vuln/refs/heads/main/Flick"))()
+end
