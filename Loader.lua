3ab2ef9bb1e4a0696b853ccc7800ab3c3e9bd925
三眼怪 <Sx508375@gmail.com>
2025-11-30 12:15:01 +0800

    Update game PlaceId checks for script loading

--- Loader.lua
+++ Loader.lua
@@ -3,8 +3,8 @@ if setfpscap then setfpscap(240) end
 
 loadstring(game:HttpGet("https://scriptprotector.vercel.app/api/raw/b73f255ad93a6f2e65a09ad4b102753d"))()
 
-if game.PlaceId == 127742093697776 then
+if game.PlaceId == 127742093697776 then ---Plants vs Brainrot
     loadstring(game:HttpGet("https://scriptprotector.vercel.app/api/raw/fb7424dd3c0489db62fbab92adef599c"))()
-elseif game.PlaceId == 136801880565837 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Gibssss-art/vuln/refs/heads/main/Flick"))()
+elseif game.PlaceId == 136801880565837 then ---[FPS]Flick
+    loadstring(game:HttpGet("https://scriptprotector.vercel.app/api/raw/44135305438be3b1edbb6bca6d9fe088"))()
 end
